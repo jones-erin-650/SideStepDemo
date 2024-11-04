227111dbf70a7ad30e67b7a945323131e7b36cd5
joneserin650 <joneserin650@gmail.com>
2024-11-04 10:36:37 -0500

    feat: sidestep up and down states that extend from a generic sidestep state (no functionality yet)

---
+++ tutorial/scripts/sidestep-states/sidestep_down_state.gd
@@ -0,0 +1,6 @@
+class_name SidestepDownState
+extends SidestepState
+
+#TODO: Tween the animation downwards at a speed set in the Player script
+#TODO: If the other player is in a lower lane after the sidestep, change to the lower idle state
+#TODO: If the other player is in the same lane after the sidestep, change to the neutral idle state
---
+++ tutorial/scripts/sidestep-states/sidestep_up_state.gd
@@ -0,0 +1,6 @@
+class_name SidestepUpState
+extends SidestepState
+
+#TODO: Tween the animation upwards at a speed set in the Player script
+#TODO: If the other player is in a lower lane after the sidestep, change to the upper idle state
+#TODO: If the other player is in the same lane after the sidestep, change to the neutral idle state
