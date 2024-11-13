17db7eadc7eaeb388f7bbd99edb05a5ef69a6f55
eribrin <joneserin650@gmail.com>
2024-11-13 03:29:59 -0500

    fix: camera properly moves on hit

--- tutorial/scripts/hitboxes/hurtbox.gd
+++ tutorial/scripts/hitboxes/hurtbox.gd
@@ -1,6 +1,8 @@
 class_name HurtBox
 extends Area2D
 
+@onready var camera: Camera = get_tree().get_first_node_in_group("Camera")
+
 func _ready() -> void:
 	collision_layer = 7
 	collision_mask = 6
@@ -11,3 +13,8 @@ func on_area_entered(hitbox: HitBox) -> void:
 #	deal damage
 	pass
 	print("damage dealt")
+
+func add_game_juice() -> void:
+	print("Adding game juice")
+	camera.set_zoom_strength(1.1)
+	camera.set_shake_strength(Vector2(5, 5))
